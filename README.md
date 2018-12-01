8f24a4899afeaef6b0d7d122bd92093786e0a3de
Daniel Ludwig <codi@code-disaster.com>
2018-12-01 14:15:51 +0100

    day 01

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2018
+
+My solutions to the [Advent of Code 2018](http://adventofcode.com/2018/about) puzzles.
---
+++ day01/src/main.rs
@@ -0,0 +1,42 @@
+use std::collections::HashSet;
+
+pub fn main() {
+
+    let input = include_str!("input.txt");
+
+    // part 1
+    {
+        let mut sum = 0;
+
+        for line in input.lines() {
+            let n = line.parse::<i32>().unwrap();
+            sum += n;
+        }
+
+        println!("sum = {}", sum);
+    }
+
+    // part 2
+    {
+        let mut sum = 0;
+        let mut freqs = HashSet::new();
+        let mut found: Option<i32> = None;
+
+        while found.is_none() {
+            for line in input.lines() {
+                let n = line.parse::<i32>().unwrap();
+                sum += n;
+
+                if freqs.contains(&sum) {
+                    found = Some(sum);
+                    break;
+                }
+
+                freqs.insert(sum);
+            }
+        }
+
+        println!("freq = {}", found.unwrap());
+    }
+
+}
